6facfe73b7f5fe427365eb129540337f5936c4f3
Gabriel Crivellaro <gabrielcrivellaro1998@gmail.com>
2025-10-16 21:34:24 -0300

    Adicionei página sobre e botão interativo com JavaScript

---
+++ script.js
@@ -0,0 +1,21 @@
+const botao = document.getElementById('modoEscuro');
+const body = document.body;
+let modoEscuro = true;
+
+botao.addEventListener('click', function() {
+    if (modoEscuro) {
+        //Volta pro modo claro
+        body.style.backgroundColor = '#f4f4f4';
+        body.style.color = 'black';
+        botao.textContent = '🌙 Modo Escuro';
+        botao.style.background = '#00d4ff';
+        modoEscuro = false;
+    } else {
+        //Ativa modo escuro
+        body.style.backgroundColor = '#1a1a1a';
+        body.style.color = 'white';
+        botao.textContent = '☀️ Modo Claro';
+        botao.style.background = '#00d4ff';
+        modoEscuro = true;
+    }
+});
